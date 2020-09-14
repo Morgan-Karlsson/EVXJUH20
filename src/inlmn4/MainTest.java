3b8d7fc85b7d324c2fbc133bad2502c6bef5c7e2
Morgan Karlsson <morgan.karlsson@utb.ecutbildning.se>
2020-09-14 07:45:50 +0200

    Added sameTime function

--- src/inlmn4/MainTest.java
+++ src/inlmn4/MainTest.java
@@ -12,6 +12,10 @@ public static String COMMA_DELIMITER = ",";
 	
 
 	public static void main(String[] args) {
+		
+		int count = 0;
+		int[] index = {0, 0};
+		int i = 0;
 
 		List<List<String>> records = new ArrayList<>();
 		try (Scanner scanner = new Scanner(new File("sample.csv"));) {
@@ -24,12 +28,15 @@ public static String COMMA_DELIMITER = ",";
 		}
 		
 		for(List<String> b:records){ 
-			System.out.println(b);  
+			System.out.println(b);
+			count += countLetters("a", b);
 			
 		}
 		
 		
+		System.out.println("Det finns " + count + " personer med bokstaven a i sitt namn");
 		
+		sameTime(records);
 
 	}
 
@@ -42,18 +49,48 @@ public static String COMMA_DELIMITER = ",";
 			}
 		}
 		
-		int count = 0;
-		
-		for(int i=0; i<values.size(); i++) {
-			
-			
-			if(values.get(1).contains("a") || values.get(2).contains("a")) 
-				count++;
-		}
-		 
-		System.out.println(count-2);
-		
 		
 		return values;
+	}
+	
+	
+	private static int countLetters(String letter, List<String> values) {
+		
+		int count = 0;
+		
+		if(!values.get(1).contains("group")) {
+			
+			
+			if(values.get(1).toLowerCase().contains("a") && values.get(2).toLowerCase().contains("a")) {
+				count = count+2;
+			} else if (values.get(1).toLowerCase().contains("a")) {
+				count++;
+			} else if (values.get(2).toLowerCase().contains("a")){
+				count++;
+			}
+		}
+		
+		return count;
+	}
+	
+	private static void sameTime(List<List<String>> l) {
+		
+		String names = new String();
+		
+		System.out.println(l.size());
+		
+		for(int i=0; i<l.size(); i++) {
+			for(int j=l.size()-1; j>i; j--) {
+				if (l.get(i).get(0).equals(l.get(j).get(0)) && !l.get(i).get(0).equals(""))  {
+					names = l.get(i).get(1) + " " + l.get(i).get(2) + " & " + l.get(j).get(1) + " " + l.get(j).get(2);
+					System.out.println(names);	
+				}
+				
+			}
+			
+		}
+		
+		
+	
 	}
 }
