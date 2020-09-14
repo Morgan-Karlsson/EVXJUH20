286d80e4719e02a0fcca1120041f3d86b1db382e
Morgan Karlsson <morgan.karlsson@utb.ecutbildning.se>
2020-09-14 07:57:30 +0200

    Added some comments

--- src/inlmn4/MainTest.java
+++ src/inlmn4/MainTest.java
@@ -14,8 +14,6 @@ public static String COMMA_DELIMITER = ",";
 	public static void main(String[] args) {
 		
 		int count = 0;
-		int[] index = {0, 0};
-		int i = 0;
 
 		List<List<String>> records = new ArrayList<>();
 		try (Scanner scanner = new Scanner(new File("sample.csv"));) {
@@ -53,19 +51,26 @@ public static String COMMA_DELIMITER = ",";
 		return values;
 	}
 	
-	
+	/*
+	 * Check columns with the names except the column that contains the word "group".
+	 * Returns an int that represents the number of the letter.
+	 */
 	private static int countLetters(String letter, List<String> values) {
 		
 		int count = 0;
 		
 		if(!values.get(1).contains("group")) {
 			
-			
-			if(values.get(1).toLowerCase().contains("a") && values.get(2).toLowerCase().contains("a")) {
+			/*
+			 * 1st check is checking both columns for the letter "a" and adds 2 to the counter.
+			 * 2nd check is if just column 1 is containing "a".
+			 * And 3rd check for column 2.
+			 */
+			if(values.get(1).toLowerCase().contains(letter) && values.get(2).toLowerCase().contains(letter)) {
 				count = count+2;
-			} else if (values.get(1).toLowerCase().contains("a")) {
+			} else if (values.get(1).toLowerCase().contains(letter)) {
 				count++;
-			} else if (values.get(2).toLowerCase().contains("a")){
+			} else if (values.get(2).toLowerCase().contains(letter)){
 				count++;
 			}
 		}
@@ -77,8 +82,12 @@ public static String COMMA_DELIMITER = ",";
 		
 		String names = new String();
 		
-		System.out.println(l.size());
-		
+		/*
+		 * Loops through all the rows in the outer loop and the rows backwards in the inner loop. The inner loop goes through each row except the one that
+		 * the outer loop is on. 
+		 * Then checks in the Timestamp column if the value matches any other value in that column. Builds a string that it then prints with the names corresponding
+		 * to the timestamp.
+		 */
 		for(int i=0; i<l.size(); i++) {
 			for(int j=l.size()-1; j>i; j--) {
 				if (l.get(i).get(0).equals(l.get(j).get(0)) && !l.get(i).get(0).equals(""))  {
@@ -90,7 +99,5 @@ public static String COMMA_DELIMITER = ",";
 			
 		}
 		
-		
-	
 	}
 }
