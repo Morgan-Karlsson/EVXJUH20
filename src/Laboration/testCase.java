b9fc9c061191b26016e59fe387578c81d8dab66d
Morgan Karlsson <morgan.karlsson@utb.ecutbildning.se>
2020-09-14 09:25:58 +0200

    Added a test case file

---
+++ src/Laboration/testCase.java
@@ -0,0 +1,14 @@
+package Laboration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testCase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
